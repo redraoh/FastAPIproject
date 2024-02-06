1781ed3ea385aa66087e42e8fa67f99462ecd426
Jang <kinskicom@gmail.com>
2024-02-06 11:50:03 +0900

    회원가입 - 프론트엔드

--- app/routes/member.py
+++ app/routes/member.py
@@ -3,6 +3,8 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
+from app.schemas.member import NewMember
+
 member_router = APIRouter()
 
 # jinja2 설정
@@ -13,6 +15,17 @@ member_router.mount('/static', StaticFiles(directory='views/static'), name='stat
 @member_router.get('/join', response_class=HTMLResponse)
 def join(req: Request):
     return templates.TemplateResponse('join.html',{'request': req})
+
+
+# DB로 제출하기 / 회원가입 완료시키기
+@member_router.post('/join')
+def joinok(req: Request, mdto: NewMember):
+    print(mdto)
+    return 1
+    # return templates.TemplateResponse('joinok.html',{'request': req})
+
+
+
 
 @member_router.get('/login', response_class=HTMLResponse)
 def login(req: Request):
