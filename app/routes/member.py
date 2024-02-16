aaebf2c5514c3f807fac0fb5942a7819e0055f74
Jang <kinskicom@gmail.com>
2024-02-16 11:17:04 +0900

    로그인 4 - 로그인 여부에 따라 처리

--- app/routes/member.py
+++ app/routes/member.py
@@ -52,4 +52,6 @@ def logout(req: Request):
 
 @member_router.get('/myinfo', response_class=HTMLResponse)
 def myinfo(req: Request):
+    if 'm' not in req.session:
+        return RedirectResponse(url='/login', status_code=status.HTTP_303_SEE_OTHER)
     return templates.TemplateResponse('myinfo.html',{'request': req})
