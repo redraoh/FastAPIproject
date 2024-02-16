f9112c893035132cfcf5361706cbc905149f6db5
Jang <kinskicom@gmail.com>
2024-02-16 10:53:26 +0900

    로그인 3 - 세션관련 미들웨어, 로그아웃

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 * jinja2 template
 * pydantic_settings
 * python-multipart
-
+* itsdangerous
 
 ## nginx 를 이용한 이미지 cdn 서버 구축
 * nginx 다운로드 및 압축해제 (/opt/homebrew/etc/nginx)
--- app/routes/member.py
+++ app/routes/member.py
@@ -44,6 +44,12 @@ def login(req: Request, userid: str = Form(), passwd: str = Form()):
     else:
         return RedirectResponse(url='/loginerror', status_code=status.HTTP_303_SEE_OTHER)
 
+
+@member_router.get('/logout')
+def logout(req: Request):
+    req.session.clear()     # 생성된 세션객체 제거
+    return RedirectResponse(url='/', status_code=status.HTTP_303_SEE_OTHER)
+
 @member_router.get('/myinfo', response_class=HTMLResponse)
 def myinfo(req: Request):
     return templates.TemplateResponse('myinfo.html',{'request': req})
